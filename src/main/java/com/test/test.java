bf5304e9922316f9cf13194c6b18c91c14311d52
weiwei <1972698267@qq.com>
2019-08-17 17:43:53 +0800

    wu

--- src/main/java/com/test/test.java
+++ src/main/java/com/test/test.java
@@ -7,5 +7,6 @@ package com.test;
 public class test {
     public static void main(String[] args) {
         System.out.println("hello world!");
+        System.out.println("hello world!");
     }
 }
