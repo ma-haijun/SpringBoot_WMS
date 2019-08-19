aa2985b4983e3364dea4a160ac7a6c618b4d220a
qin yi <1615155306@qq.com>
2019-08-19 09:07:13 +0800

    测试yu

--- src/main/java/com/test/test.java
+++ src/main/java/com/test/test.java
@@ -8,5 +8,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello world!");
         System.out.println("hello world!");
+        System.err.println("aaaaaa");
     }
 }
